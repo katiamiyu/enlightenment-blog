99935d40b60f83cd3dd96609db2505e726e49053
katiamiyu <katiamiyu@gmail.com>
2024-09-02 11:17:21 +0100

    updated

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const app = express();
 app.get('/', (req, res) => {
   console.log(res);
   res.send('<h1>Hello world</h1>');
-})
+});
 
 app.listen(3000, () => {
   console.log('listening at port 3000');
-})
+});
