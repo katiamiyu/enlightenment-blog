503dfbed7139c24678a64856582ef0c60afcc383
katiamiyu <katiamiyu@gmail.com>
2024-09-06 11:37:48 +0100

    put route

--- app.js
+++ app.js
@@ -6,8 +6,38 @@ let posts = require('./models/posts');
 app.use(express.json());
 
 app.get("/api/v1/posts", (req, res) => {
-  res.json(posts);
+  res.json({
+    status: 200,
+    data: posts,
+  });
 });
+
+app.get("/api/v1/posts/:id", (req, res) => {
+  const { id } = req.params;
+  const post = posts.find((post) => post.id == id);
+  res.json({
+    status: 200,
+    data: post,
+  });
+});
+
+app.post("/api/v1/posts/", (req, res) => {
+  const { id, title, body, userid, date } = req.body;
+  posts.push({ id, title, body, userid, date });
+  res.json({
+    status: 201,
+    data: posts,
+  });
+})
+
+app.put("/api/v1/posts/:id", (req, res) => {
+  const { id } = req.params;
+  posts.push({ id, title, body, userid, date });
+  res.json({
+    status: 201,
+    data: posts,
+  });
+})
 
 app.listen(3000,"127.0.0.1", () => {
   console.log('listening at port 3000');
--- models/posts.js
+++ models/posts.js
@@ -5,6 +5,13 @@ posts = [
     body: "thed  iuigu ihudo ubul i guik hs vyjmuh ukhu",
     userid: 1,
     date: "13-03-1988"
+  },
+  {
+    id: 2,
+    title: "Niga Ninga",
+    body: "thed  iuigu ihudo ubul i guik hs vyjmuh ukhu",
+    userid: 1,
+    date: "13-08-1988"
   }
 ]
 module.exports = posts;
