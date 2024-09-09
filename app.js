4117825a08ef7b7efb159eb9808b9e4b0aeab8bb
katiamiyu <katiamiyu@gmail.com>
2024-09-09 20:39:52 +0100

    database_setup_dotenv_file

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const app = express();
 const posts = require('./routes/posts');
+const connectDB = require('./db/connect');
+require('dotenv').config();
+const port = process.env.PORT;
+
 
 // middleware
 app.use(express.json());
 app.use('/api/v1/posts', posts);
 
-app.listen(3000,"127.0.0.1", () => {
-  console.log('listening at port 3000');
-});
+const startUp = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+    console.log('connected to server');
+    app.listen(port,() => console.log(`listening at port ${port}`)); 
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// spinup server
+startUp();
